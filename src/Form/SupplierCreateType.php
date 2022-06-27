69500dd127ce98d6e1c73fb0ed357f31ade85815
Trung <kientrung2002.kn@gmail.com>
2022-06-27 13:36:53 +0700

    CreateSupplier

---
+++ src/Form/SupplierCreateType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Supplier;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SupplierCreateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Name', TextType::class)
+            ->add('Address', TextType::class)
+            ->add('Phone', TextareaType::class);
+
+
+
+    }
+
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => supplier::class,
+        ]);
+    }
+}
